05279fb4aa3899717759b28f505edb69bc638257
Odysseyheimer <nopskunggmaer24@gmail.com>
2026-07-21 17:28:42 +0700

    v102: animated cartoon layer for Campaign — toon.js/css (emoji characters), NPC scene w/ speech bubble + talk, hero walker on map, win confetti + jump, node pulse, transitions; +hero/npcFace data

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 */
 
 /* eslint-disable no-var */
-var CACHE = "pocket-tutor-v101";
+var CACHE = "pocket-tutor-v102";
 
 // tiny, must-always-work shell — safe to precache atomically
 var SHELL_URLS = [
