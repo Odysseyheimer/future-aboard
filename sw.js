344d4013b0fa5f4acb1c70b28bef6f474426866b
Odysseyheimer <nopskunggmaer24@gmail.com>
2026-08-01 12:12:27 +0700

    v109: pagination for Formal mode lists (Map 20/pg, Word List 12/pg, Articles 12/pg) — prev/next + page N/M, resets on filter, scrolls to top

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 */
 
 /* eslint-disable no-var */
-var CACHE = "pocket-tutor-v108";
+var CACHE = "pocket-tutor-v109";
 
 // tiny, must-always-work shell — safe to precache atomically
 var SHELL_URLS = [
