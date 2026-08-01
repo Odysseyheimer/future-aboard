7ff9d232537db89b5aae7bc315f6e42524252e46
Odysseyheimer <nopskunggmaer24@gmail.com>
2026-08-01 11:35:46 +0700

    v106: expand Formal English dataset 255->429 verbs (advanced professional: negotiate, refactor, provision, escalate, aggregate...); 1239 unique formal words

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 */
 
 /* eslint-disable no-var */
-var CACHE = "pocket-tutor-v105";
+var CACHE = "pocket-tutor-v106";
 
 // tiny, must-always-work shell — safe to precache atomically
 var SHELL_URLS = [
